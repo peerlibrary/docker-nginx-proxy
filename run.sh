37aee39b0aaf100fbcf3bf5f969f90c92d1289a6
Mitar <mitar.git@tnode.com>
2014-07-06 20:32:09 -0700

    Prepare directories.

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #/bin/bash -e
 
+mkdir -p /srv/var/log/nginx
+mkdir -p /srv/var/log/dockergen
+chown nobody:nogroup /srv/var/log/dockergen
+
 docker run -d -p 80:80/tcp --name nginx -v /var/run/docker.sock:/docker.sock -v /srv/var/log/nginx:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/nginx-proxy
