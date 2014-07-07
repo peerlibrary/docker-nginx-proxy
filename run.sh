7ac919f322acf0f1a600dfe5030c063517c8f00e
Mitar <mitar.git@tnode.com>
2014-07-06 20:38:47 -0700

    Chown inside container.

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 
 mkdir -p /srv/var/log/nginx
 mkdir -p /srv/var/log/dockergen
-chown nobody:nogroup /srv/var/log/dockergen
 
 docker run -d -p 80:80/tcp --name nginx -v /var/run/docker.sock:/docker.sock -v /srv/var/log/nginx:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/nginx-proxy
