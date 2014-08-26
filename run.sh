bfed90e8f6a59940b1786fa510804cf462e4bc59
Mitar <mitar.git@tnode.com>
2014-08-26 04:37:13 -0700

    Moved to PeerLibrary.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash -e
 
-mkdir -p /srv/var/log/nginx
-mkdir -p /srv/var/log/dockergen
+mkdir -p /srv/log/nginx
+mkdir -p /srv/log/dockergen
 
-docker run -d -p 80:80/tcp --name nginx -v /var/run/docker.sock:/docker.sock -v /srv/var/log/nginx:/var/log/nginx -v /srv/var/log/dockergen:/var/log/dockergen cloyne/nginx-proxy
+docker run -d -p 80:80/tcp --name nginx -v /var/run/docker.sock:/docker.sock -v /srv/log/nginx:/var/log/nginx -v /srv/log/dockergen:/var/log/dockergen cloyne/nginx-proxy
